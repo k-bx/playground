255be802ef7ad39ee507cb8ea48ebffa90bc2ea8
Kostiantyn Rybnikov <k-bx@k-bx.com>
2019-11-13 17:38:13 +0200

    Searching for topologies

--- topology-hs/search_topologies.hs
+++ topology-hs/search_topologies.hs
@@ -12,128 +12,76 @@ findTopologies :: Ord a => Set a -> Set (Topology a)
 findTopologies xs = S.filter (isTopology xs) (genAllSubsets (genAllSubsets xs))
 
 genAllSubsets :: Ord a => Set a -> Set (Set a)
-genAllSubsets xs = S.fromList $ map S.fromList $ Data.List.subsequences $ S.toList xs
+genAllSubsets xs =
+  S.fromList $ map S.fromList $ Data.List.subsequences $ S.toList xs
 
 isTopology :: Ord a => Set a -> Set (Set a) -> Bool
 isTopology xs tts =
   (S.empty `S.member` tts && xs `S.member` tts) &&
-  (any
+  (all
      (== True)
-     [ S.union ts1 ts2 == ts3
+     [S.union ts1 ts2 `S.member` tts | ts1 <- S.toList tts, ts2 <- S.toList tts]) &&
+  (all
+     (== True)
+     [ (S.intersection ts1 ts2) `S.member` tts
      | ts1 <- S.toList tts
      , ts2 <- S.toList tts
-     , ts3 <- S.toList tts
-     ]) &&
-  (any
-     (== True)
-     [ (S.intersection ts1 ts2) == ts3
-     | ts1 <- S.toList tts
-     , ts2 <- S.toList tts
-     , ts3 <- S.toList tts
      ])
 
 main :: IO ()
 main = do
-  let res = 
+  let res =
         S.fromList
           ([ tts
            | tts <- S.toList (findTopologies (S.fromList ['a', 'b', 'c', 'd']))
            , length tts == 4
            ])
   print $ length res
+  print $ Data.List.sort $ map (map S.toList) $ map S.toList $ S.toList res
 
--- length == 91
-
--- fromList
---   [ fromList [fromList "", fromList "a", fromList "ab", fromList "abcd"]
---   , fromList [fromList "", fromList "a", fromList "abc", fromList "abcd"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "abd"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "ac"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "acd"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "ad"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "b"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "bc"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "bcd"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "bd"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "c"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "cd"]
---   , fromList [fromList "", fromList "a", fromList "abcd", fromList "d"]
---   , fromList [fromList "", fromList "ab", fromList "abc", fromList "abcd"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "abd"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "ac"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "acd"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "ad"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "b"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "bc"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "bcd"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "bd"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "c"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "cd"]
---   , fromList [fromList "", fromList "ab", fromList "abcd", fromList "d"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "abd"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "ac"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "acd"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "ad"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "b"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "bc"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "bcd"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "bd"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "c"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "cd"]
---   , fromList [fromList "", fromList "abc", fromList "abcd", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "ac"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "acd"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "ad"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "b"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "bc"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "bcd"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "abd", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "acd"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "ad"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "b"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "bc"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "bcd"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "ac", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "ad"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "b"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "bc"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "bcd"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "acd", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "b"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "bc"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "bcd"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "ad", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "b", fromList "bc"]
---   , fromList [fromList "", fromList "abcd", fromList "b", fromList "bcd"]
---   , fromList [fromList "", fromList "abcd", fromList "b", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "b", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "b", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "b", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "bc", fromList "bcd"]
---   , fromList [fromList "", fromList "abcd", fromList "bc", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "bc", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "bc", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "bc", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "bcd", fromList "bd"]
---   , fromList [fromList "", fromList "abcd", fromList "bcd", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "bcd", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "bcd", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "bd", fromList "c"]
---   , fromList [fromList "", fromList "abcd", fromList "bd", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "bd", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "c", fromList "cd"]
---   , fromList [fromList "", fromList "abcd", fromList "c", fromList "d"]
---   , fromList [fromList "", fromList "abcd", fromList "cd", fromList "d"]
---   ]
+-- 43
+--
+-- [ ["", "a", "ab", "abcd"]
+-- , ["", "a", "abc", "abcd"]
+-- , ["", "a", "abcd", "abd"]
+-- , ["", "a", "abcd", "ac"]
+-- , ["", "a", "abcd", "acd"]
+-- , ["", "a", "abcd", "ad"]
+-- , ["", "a", "abcd", "bcd"]
+-- , ["", "ab", "abc", "abcd"]
+-- , ["", "ab", "abcd", "abd"]
+-- , ["", "ab", "abcd", "b"]
+-- , ["", "ab", "abcd", "cd"]
+-- , ["", "abc", "abcd", "ac"]
+-- , ["", "abc", "abcd", "b"]
+-- , ["", "abc", "abcd", "bc"]
+-- , ["", "abc", "abcd", "c"]
+-- , ["", "abc", "abcd", "d"]
+-- , ["", "abcd", "abd", "ad"]
+-- , ["", "abcd", "abd", "b"]
+-- , ["", "abcd", "abd", "bd"]
+-- , ["", "abcd", "abd", "c"]
+-- , ["", "abcd", "abd", "d"]
+-- , ["", "abcd", "ac", "acd"]
+-- , ["", "abcd", "ac", "bd"]
+-- , ["", "abcd", "ac", "c"]
+-- , ["", "abcd", "acd", "ad"]
+-- , ["", "abcd", "acd", "b"]
+-- , ["", "abcd", "acd", "c"]
+-- , ["", "abcd", "acd", "cd"]
+-- , ["", "abcd", "acd", "d"]
+-- , ["", "abcd", "ad", "bc"]
+-- , ["", "abcd", "ad", "d"]
+-- , ["", "abcd", "b", "bc"]
+-- , ["", "abcd", "b", "bcd"]
+-- , ["", "abcd", "b", "bd"]
+-- , ["", "abcd", "bc", "bcd"]
+-- , ["", "abcd", "bc", "c"]
+-- , ["", "abcd", "bcd", "bd"]
+-- , ["", "abcd", "bcd", "c"]
+-- , ["", "abcd", "bcd", "cd"]
+-- , ["", "abcd", "bcd", "d"]
+-- , ["", "abcd", "bd", "d"]
+-- , ["", "abcd", "c", "cd"]
+-- , ["", "abcd", "cd", "d"]
+-- ]
