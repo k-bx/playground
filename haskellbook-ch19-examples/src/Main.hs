288baf6981e13a596e1d7bc7ef629dc7117f11b5
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-02-08 13:48:39 +0200

    Scotty example

--- haskellbook-ch19-examples/src/Main.hs
+++ haskellbook-ch19-examples/src/Main.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import System.Environment
+import Web.Scotty
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  case args of
+    ["scotty"] -> mainScotty
+    _ -> putStrLn "howdy!"
+
+mainScotty :: IO ()
+mainScotty =
+  scotty 3000 $ do
+    get "/:word" $ do
+      beam <- param "word"
+      html (mconcat ["<h1>Scotty, ", beam, " me up!</h1>"])
