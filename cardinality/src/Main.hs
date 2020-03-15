8c3e0be391fe23ec0c5f257cb5d2bf265c87e378
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-03-15 18:12:08 +0200

    Cardinality

--- cardinality/src/Main.hs
+++ cardinality/src/Main.hs
@@ -1,5 +1,77 @@
 module Main where
 
+data Tri = T1 | T2 | T3
+
+six :: [(Tri, Bool)]
+six =
+  [ (T1, False),
+    (T1, True),
+    (T2, False),
+    (T2, True),
+    (T3, False),
+    (T3, True)
+  ]
+
+eight :: [Tri -> Bool]
+eight =
+  [ \t -> case t of
+      T1 ->
+        False
+      T2 ->
+        False
+      T3 ->
+        False,
+    \t -> case t of
+      T1 ->
+        False
+      T2 ->
+        False
+      T3 ->
+        True,
+    \t -> case t of
+      T1 ->
+        False
+      T2 ->
+        True
+      T3 ->
+        False,
+    \t -> case t of
+      T1 ->
+        False
+      T2 ->
+        True
+      T3 ->
+        True,
+    \t -> case t of
+      T1 ->
+        True
+      T2 ->
+        False
+      T3 ->
+        False,
+    \t -> case t of
+      T1 ->
+        True
+      T2 ->
+        False
+      T3 ->
+        True,
+    \t -> case t of
+      T1 ->
+        True
+      T2 ->
+        True
+      T3 ->
+        False,
+    \t -> case t of
+      T1 ->
+        True
+      T2 ->
+        True
+      T3 ->
+        True
+  ]
+
 main :: IO ()
 main = do
   putStrLn "hello world"
