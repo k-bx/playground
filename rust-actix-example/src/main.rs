8f5071452076d3d8b80b37af663696733e7ad293
Kostiantyn Rybnikov <k-bx@k-bx.com>
2020-05-14 18:21:29 +0300

    Json response

--- rust-actix-example/src/main.rs
+++ rust-actix-example/src/main.rs
@@ -17,6 +17,23 @@ async fn test2(test2data: web::Json<Test2Data>) -> impl Responder {
     format!("Hello! {:?}", test2data)
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Test3Req {
+    hello: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Test3Resp {
+    world: String,
+}
+
+#[post("/api/test3.json")]
+async fn test3(inp: web::Json<Test3Req>) -> actix_web::Result<web::Json<Test3Resp>> {
+    Ok(web::Json(Test3Resp {
+        world: inp.hello.clone(),
+    }))
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
@@ -28,6 +45,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(test1)
             .service(test2)
+            .service(test3)
     })
     .bind("127.0.0.1:8080")?
     .run()
